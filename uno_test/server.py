242075aaef04e65ec6fdb21ffb5cfa11b5a1f7c7
danielcruz0913 <dcruz2@gmail.com>
2023-01-06 16:13:50 -0500

    updated server.py

--- uno_test/server.py
+++ uno_test/server.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import socket
 
 class Server:
@@ -8,6 +7,3 @@ class Server:
         self.svr = "10.17.3.65"
         # This allows us to send and receive data
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-=======
-
->>>>>>> 97d49f1d14f6241d879fb07e17c9e72855f6c0fd
