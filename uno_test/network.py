4d696178741aaf63d23eb9d14885819aade6617c
danielcruz0913 <dcruz2@gmail.com>
2023-01-11 14:23:23 -0500

    Changed some of the names of the variables

--- uno_test/network.py
+++ uno_test/network.py
@@ -3,9 +3,9 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "132.162.25.81"
-        self.portNum = 5555
-        self.address = (self.server, self.portNum)
+        self.host = "localhost"
+        self.portNum = 9889
+        self.address = (self.host, self.portNum)
 
         self.id = self.connect()
         print(self.id)
@@ -13,7 +13,7 @@ class Network:
     def connect(self):
         try:
             # Here we attempt to connect
-            self.client.settimeout(5.0)
+            self.client.settimeout(5.0) # will wait 5 seconds for a connection; if not found, disconnects
             self.client.connect(self.address)
             return self.client.recv(2048).decode
         except socket.error as e:
