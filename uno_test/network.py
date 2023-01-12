0ac227a945cb88aa671605dc8253badd1072c14a
danielcruz0913 <dcruz2@gmail.com>
2023-01-11 22:21:25 -0500

    Added a function; program does not work yet

--- uno_test/network.py
+++ uno_test/network.py
@@ -7,7 +7,7 @@ class Network:
         self.portNum = 9889
         self.address = (self.host, self.portNum)
         self.id = self.connect()
-        print(str(self.id))
+        print(self.id)
 
     def connect(self):
         try:
@@ -17,9 +17,19 @@ class Network:
             return self.client.recv(2048).decode
         except socket.error as e:
             print(str(e))
-            print("Could not connect")
+            print(f"Could not connect to:  {self.address}")
+
+    def send(self, msg):
+        try:
+            self.client.send(str.encode(msg))
+            return self.client.recv(2048).decode
+        except socket.error as e:
+            print(str(e))
+            print("Could not send message")
 
 def main():
     network = Network()
-
+    print(network.send("This"))
+    print(network.send("works"))
+    
 main()
