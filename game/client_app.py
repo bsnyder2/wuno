fff89b9b9adb3cb5f158d1b22e5b8cfc79766a63
Ben Snyder <119763240+benv2s@users.noreply.github.com>
2023-01-23 15:03:59 -0500

    Split app.py into client_app and server_app

--- game/client_app.py
+++ game/client_app.py
@@ -1,16 +1,9 @@
 import data.client
-import data.server
-import display.gui
 
-# display.gui.GUI()
 
 def main():
     HOST_IP = "10.17.50.224"
     PORT = 50000
-
-    # start server
-    # sv = data.server.Server(HOST_IP, PORT)
-    # sv.start()
 
     # start client
     c = data.client.Client(HOST_IP, PORT)
@@ -20,5 +13,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# central server should store one game instance: and multiple GUIs
