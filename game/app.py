69b01647f6a58c13e73a446491b1d7b50ca35007
Ben Snyder <119763240+benv2s@users.noreply.github.com>
2023-01-23 14:28:39 -0500

    Put server and client init in same file

--- game/app.py
+++ game/app.py
@@ -1,18 +1,24 @@
+import data.client
 import data.server
 import display.gui
 
-display.gui.GUI()
+# display.gui.GUI()
+
+def main():
+    HOST_IP = "10.17.50.224"
+    PORT = 50000
+
+    # start server
+    # sv = data.server.Server(HOST_IP, PORT)
+    # sv.start()
+
+    # start client
+    c = data.client.Client(HOST_IP, PORT)
+    c.connect()
+    c.send("hello")
 
 
-# def main():
-#     HOST_IP = "10.17.50.224"
-#     PORT = 50000
-
-#     sv = data.server.Server(HOST_IP, PORT)
-#     sv.start()
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 # central server should store one game instance: and multiple GUIs
