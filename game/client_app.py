9114fab52ba5f047d6633dfde1f5ec1589a911eb
Ben Snyder <119763240+benv2s@users.noreply.github.com>
2023-01-23 15:21:39 -0500

    Reverted imports in app files

--- game/client_app.py
+++ game/client_app.py
@@ -1,13 +1,15 @@
 import data.client
+import display.gui
 
 
 def main():
-    HOST_IP = "10.17.50.224"
+    HOST_IP = "10.17.61.187"
     PORT = 50000
 
     # start client
     c = data.client.Client(HOST_IP, PORT)
     c.connect()
+
     c.send("hello")
 
 
